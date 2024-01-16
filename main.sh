e99e995df4b0dff59f63e4ca0beb6212b7587f7f
1ris_W <ediath462@gmail.com>
2024-01-16 20:35:22 +0800

    Create main.sh
    add main code

---
+++ main.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+for app in /Applications/*.app; do
+    framework_dir="$app/Contents/Frameworks"
+    if [ -d "$framework_dir" ]; then
+        for item in "$framework_dir"/*; do
+            item_name=$(basename "$item")
+            # echo $item_name
+            if [[ "$item_name" == *"Electron"* ]]; then
+                echo "Electron-based app found: $app"
+                break
+            fi
+        done
+    fi
+done
